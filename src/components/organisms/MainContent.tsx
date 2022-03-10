ebf95c17e2b01742a57d87aaa8476e13912fb724
Pedro Sousa <pedrossm2000@gmail.com>
2022-03-10 04:08:11 -0300

    fix typescript error

--- src/components/organisms/MainContent.tsx
+++ src/components/organisms/MainContent.tsx
@@ -1,15 +1,12 @@
 import { Filters, Results } from "@molecules";
-import { useCurrentResults } from "hooks";
 import React from "react";
 import styled from "styled-components";
 
 export default function MainContent() {
-  const results = useCurrentResults();
-
   return (
     <StyledMain>
       <Filters />
-      <Results {...results} />
+      <Results />
     </StyledMain>
   );
 }
