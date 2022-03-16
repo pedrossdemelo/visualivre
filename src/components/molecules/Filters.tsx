92057f9634b28ac1910a078d3c1233efed047975
Pedro Sousa <pedrossm2000@gmail.com>
2022-03-16 02:54:58 -0300

    build on vercel these fixes lol

--- src/components/molecules/Filters.tsx
+++ src/components/molecules/Filters.tsx
@@ -12,6 +12,7 @@ import React, { memo } from "react";
 import styled from "styled-components";
 
 function Filters() {
+  // TODO: Implement conditional rendering in CSS
   const smallScreen = useMediaQuery("(max-width: 700px)");
 
   return (
